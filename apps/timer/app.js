664bb997afcbcc86527a5327f5ade5719f5b621a
Adib <94355233+Adib49@users.noreply.github.com>
2023-08-03 01:52:27 +0200

    Update app.js

--- apps/timer/app.js
+++ apps/timer/app.js
@@ -1,12 +1,10 @@
 
 // Display the menu options
 function displayMenu() {
-  E.showMessage('\n===== MENU =====');
   E.showMessage('1. Option 1');
   E.showMessage('2. Option 2');
   E.showMessage('3. Option 3');
-  E.showMessage('================\n');
-}
+ }
 
 // Start the application by displaying the menu
 displayMenu();
