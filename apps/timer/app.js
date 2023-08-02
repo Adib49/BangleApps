a4f6dafb7568362a9b3b786a3f59869a5deb51c6
Adib <94355233+Adib49@users.noreply.github.com>
2023-08-03 01:38:06 +0200

    Update app.js

--- apps/timer/app.js
+++ apps/timer/app.js
@@ -1,13 +1,41 @@
-Bangle.loadWidgets();
-Bangle.drawWidgets();
+// Importing required modules
+const readline = require('readline');
 
+// Creating a readline interface to read user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-function showMainMenu() {
- const mainmenu = {
-    '': { 'title': /*LANG*/'Recorder' },
-    '< Back'
-    /*LANG*/'RECORD'
-    /*LANG*/'File' 
-    /*LANG*/'View Tracks'
-    /*LANG*/'Time Period'
-  };
+// Display the menu options
+function displayMenu() {
+  console.log('\n===== MENU =====');
+  console.log('1. Option 1');
+  console.log('2. Option 2');
+  console.log('3. Option 3');
+  console.log('================\n');
+  rl.question('Enter your choice (1/2/3): ', handleMenuChoice);
+}
+
+// Handle user choice
+function handleMenuChoice(choice) {
+  switch (choice) {
+    case '1':
+      console.log('You selected Option 1.');
+      break;
+    case '2':
+      console.log('You selected Option 2.');
+      break;
+    case '3':
+      console.log('You selected Option 3.');
+      break;
+    default:
+      console.log('Invalid choice. Please choose a valid option.');
+      break;
+  }
+
+  rl.close();
+}
+
+// Start the application by displaying the menu
+displayMenu();
